1523f56e452f18dafe039dba7431184476b823db
Albert Xiao <albertx7806@gmail.com>
2019-11-23 14:24:20 -0800

    Add squad approval & rejection endpoints

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, request
+import requests
+from flask import Flask, request, redirect, url_for
 import threading
 from firebase_admin import credentials, firestore, initialize_app
 import email_util
@@ -22,17 +23,15 @@ def message_send():
     from_id = request.form['from_id']
     from_email = request.form['from_email']
 
-    def send():
+    def inner():
         # TODO: use ML to check sentiment of `msg`
         ml_approved = False
 
-        # TODO: DB lookup recipient's email using `to_id`
-        email = 'squaddtalk@gmail.com'
         # TODO: DB lookup names of `to_id` and `from_id`
         to_name = 'Albert'
         from_name = 'Tony'
         if ml_approved:
-            email_util.send_email(email, f'New Message from {from_name}', msg, f'<p>{msg}</p>')
+            send(msg, to_id, from_id, from_email)
         else:
             # TODO: DB lookup recipient's squad's emails using `to_id`
             squad_emails = ['squaddtalk@gmail.com']
@@ -40,9 +39,39 @@ def message_send():
                 email_util.send_email(a, f'Approval for Message from {from_name} to {to_name}', msg,
                                       '<p>msg</p>')
 
-    threading.Thread(target=send, daemon=True).start()
+    threading.Thread(target=inner, daemon=True).start()
 
     return {}
+
+
+@app.route('/approve', methods=('POST',))
+def approve():
+    msg = request.form['msg']
+    to_id = request.form['to_id']
+    from_id = request.form['from_id']
+    from_email = request.form['from_email']
+
+    send(msg, to_id, from_id, from_email)
+
+    # TODO: redirect to page on frontend
+    return redirect('https://google.com')
+
+
+def send(msg, to_id, from_id, from_email):
+    # TODO: DB lookup recipient's email using `to_id`
+    email = 'squaddtalk@gmail.com'
+
+    # TODO: DB lookup names of `to_id` and `from_id`
+    to_name = 'Albert'
+    from_name = 'Tony'
+
+    email_util.send_email(email, f'Message from {from_name}', msg, f'<p>{msg}</p>')
+
+
+@app.route('/reject')
+def reject():
+    # TODO: redirect to page on frontend
+    return redirect('https://google.com')
 
 
 port = int(os.environ.get('PORT', 8080))
