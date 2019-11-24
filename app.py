f03a31283c806c4570f8117157f26a9234f14d3e
Albert Xiao <albertx7806@gmail.com>
2019-11-24 07:05:08 -0800

    Revert "Custom domain email!"
    This reverts commit 4418e7250a8c2d08e6c7f02c99777a89c2ac1d39.

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import ml_approve
 app = Flask(__name__)
 cors = CORS(app)
 
+email_util.init()
+
 # Initialize Firestore DB
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
